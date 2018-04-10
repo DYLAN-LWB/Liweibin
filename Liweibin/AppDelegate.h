99142fc0640659d888645e8a0fa76906480a2565
李伟宾 <dylan_lwb@163.com>
2018-04-10 10:11:20 +0800

    Initial Commit

---
+++ Liweibin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Liweibin
+//
+//  Created by 李伟宾 on 2018/4/10.
+//  Copyright © 2018年 李伟宾. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
